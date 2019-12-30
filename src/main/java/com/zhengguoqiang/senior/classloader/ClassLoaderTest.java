16bee165e42291f4d24c0cf062786ec061f9917b
zhengguoqiang <zhengguoqiang927@gmail.com>
2019-12-30 14:24:42 +0800

    类加载

---
+++ src/main/java/com/zhengguoqiang/senior/classloader/ClassLoaderTest.java
@@ -0,0 +1,43 @@
+package com.zhengguoqiang.senior.classloader;
+
+import com.alibaba.fastjson.JSON;
+import com.zhengguoqiang.utils.FileHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.log4j.BasicConfigurator;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.security.SecureClassLoader;
+import java.util.List;
+
+/**
+ * @author zhengguoqiang
+ */
+@Slf4j
+public class ClassLoaderTest {
+
+    public static void main(String[] args) throws IOException {
+        BasicConfigurator.configure();
+        String path = "/Users/zhengguoqiang/Server/scf/service/deploy/commercialPostServer";
+//        List<String> uniqueLibPath = FileHelper.getUniqueLibPath(path);
+//        log.info(JSON.toJSONString(uniqueLibPath));
+        GlobalClassLoader.addSystemClassPathFolder(path);
+        DynamicClassLoader classLoader = new DynamicClassLoader();
+        try {
+            Class<?> aClass = classLoader.loadClass("com.bj58.fang.commercial.post.Init");
+            Object o = aClass.newInstance();
+            log.info("Hash:{}",o.hashCode());
+            System.out.println(aClass.getSimpleName());
+            Method[] methods = aClass.getDeclaredMethods();
+            System.out.println(JSON.toJSONString(methods));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class DynamicClassLoader extends SecureClassLoader{
+    public DynamicClassLoader(){
+    }
+}
---
+++ src/main/java/com/zhengguoqiang/senior/classloader/GlobalClassLoader.java
@@ -0,0 +1,54 @@
+package com.zhengguoqiang.senior.classloader;
+
+import com.zhengguoqiang.utils.FileHelper;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.List;
+
+/**
+ * @author zhengguoqiang
+ */
+@Slf4j
+public class GlobalClassLoader {
+    private static Method addURL;
+    private static URLClassLoader system;
+
+    static {
+        try {
+            addURL = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+        addURL.setAccessible(true);
+        system = (URLClassLoader) getSystemClassLoader();
+    }
+
+    private static ClassLoader getSystemClassLoader(){
+        return ClassLoader.getSystemClassLoader();
+    }
+
+    public static void addURL2SystemClassLoader(URL url){
+        try {
+            log.info("append jar to classpath:" + url.toString());
+            addURL.invoke(system,url);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addSystemClassPathFolder(String... dirs) throws IOException {
+        List<String> uniqueLibPath = FileHelper.getUniqueLibPath(dirs);
+        for (String path:uniqueLibPath){
+            URL url = new URL("file","",path);
+            addURL2SystemClassLoader(url);
+        }
+    }
+}
