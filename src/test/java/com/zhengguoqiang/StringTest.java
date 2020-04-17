f443f80acf23b2fc39ec596dde41614d66951fbd
zhengguoqiang927 <zhengguoqiang927@gmail.com>
2020-04-17 23:10:47 +0800

    add test

---
+++ src/test/java/com/zhengguoqiang/StringTest.java
@@ -0,0 +1,15 @@
+package com.zhengguoqiang;
+
+import org.junit.Test;
+
+public class StringTest {
+    private static final String a = "123";
+    private static final String b = "123";
+
+    @Test
+    public void test(){
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+        int[] array = new int[]{};
+    }
+}
