07c788a45e6ab93244164532b78aa3cd10b856c2
zhengguoqiang927 <zhengguoqiang927@gmail.com>
2020-04-25 00:14:21 +0800

    modify core-java

---
+++ src/main/java/com/zhengguoqiang/java8/exception/StackTraceTest.java
@@ -0,0 +1,29 @@
+package com.zhengguoqiang.java8.exception;
+
+import java.util.Scanner;
+
+public class StackTraceTest {
+    public static int factorial(int n){
+        System.out.println("factorial(" + n + "):");
+        Throwable t = new Throwable();
+        StackTraceElement[] stackTraceElements = t.getStackTrace();
+        for (StackTraceElement element:stackTraceElements){
+            System.out.println(element);
+        }
+        int r;
+        if (n<=1){
+            r = 1;
+        }else {
+            r = n * factorial(n-1);
+        }
+        System.out.println("return " + r);
+        return r;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n = in.nextInt();
+        factorial(n);
+    }
+}
