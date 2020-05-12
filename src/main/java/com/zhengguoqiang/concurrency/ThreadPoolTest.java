b0fcd4af12a5f1c2c32e0d23e4efc8396f84ff35
zhengguoqiang <zhengguoqiang927@gmail.com>
2020-05-12 20:28:27 +0800

    add concurrency

---
+++ src/main/java/com/zhengguoqiang/concurrency/ThreadPoolTest.java
@@ -0,0 +1,94 @@
+package com.zhengguoqiang.concurrency;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class ThreadPoolTest {
+
+    public static void main(String[] args) {
+        try(Scanner in = new Scanner(System.in)){
+            System.out.println("Enter base directory (e.g. /usr/local/jdk5.0/src): ");
+            String directory = in.nextLine();
+            System.out.print("Enter keyword (e.g. volatile): ");
+            String keyword = in.nextLine();
+
+            ExecutorService pool = Executors.newCachedThreadPool();
+
+            MatchCounter counter = new MatchCounter(new File(directory), keyword, pool);
+            Future<Integer> result = pool.submit(counter);
+
+            try {
+                System.out.println(result.get() + " matching files.");
+            }catch (ExecutionException e){
+                e. printStackTrace();
+             }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            pool.shutdown() ;
+            int largestPoolSize = ((ThreadPoolExecutor) pool).getLargestPoolSize();
+            System.out.println("largest pool size=" + largestPoolSize);
+        }
+    }
+}
+
+class MatchCounter implements Callable<Integer>{
+
+    private File directory;
+    private String keyword;
+    private ExecutorService pool;
+    private int count;
+
+    public MatchCounter(File directory, String keyword, ExecutorService pool) {
+        this.directory = directory;
+        this.keyword = keyword;
+        this.pool = pool;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        count = 0;
+        try{
+            File[] files = directory.listFiles();
+            List<Future<Integer>> results = new ArrayList<>();
+            for (File file:files){
+                if (file.isDirectory()){
+                    MatchCounter counter = new MatchCounter(file,keyword,pool);
+                    Future<Integer> submit = pool.submit(counter);
+                    results.add(submit);
+                }else {
+                    if (search(file)) count++;
+                }
+            }
+
+            for (Future<Integer> future:results){
+                try{
+                    count += future.get();
+                }catch (ExecutionException e){
+                    e.printStackTrace();
+                }
+            }
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+    public boolean search(File file){
+        try{
+            try(Scanner in = new Scanner(file,"UTF-8")){
+                boolean found = false;
+                while (!found && in.hasNextLine()){
+                    String line = in.nextLine();
+                    if (line.contains(keyword)) found = true;
+                }
+                return found;
+            }
+        }catch (IOException e){
+            return false;
+        }
+    }
+}
