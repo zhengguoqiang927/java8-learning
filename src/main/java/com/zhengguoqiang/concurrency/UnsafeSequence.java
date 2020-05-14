927040b84fa9d7d16e0f9d410bf4709b63dbf01c
zhengguoqiang927 <zhengguoqiang927@gmail.com>
2020-05-15 00:03:03 +0800

    add concurrency

---
+++ src/main/java/com/zhengguoqiang/concurrency/UnsafeSequence.java
@@ -0,0 +1,17 @@
+package com.zhengguoqiang.concurrency;
+
+public class UnsafeSequence {
+    private int value;
+
+    public int getNext(){
+        return value++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        UnsafeSequence sequence = new UnsafeSequence();
+        for (int i=0;i<20000;i++){
+            new Thread(()-> sequence.getNext()).start();
+        }
+        System.out.println(sequence.getNext());
+    }
+}
