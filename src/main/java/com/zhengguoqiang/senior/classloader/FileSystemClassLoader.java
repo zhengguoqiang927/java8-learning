2298cc3e1cf0d445eee7636c36290ad19db2340c
zhengguoqiang <zhengguoqiang927@gmail.com>
2020-12-05 19:57:55 +0800

    add FileSystemClassLoader comments

--- src/main/java/com/zhengguoqiang/senior/classloader/FileSystemClassLoader.java
+++ src/main/java/com/zhengguoqiang/senior/classloader/FileSystemClassLoader.java
@@ -4,6 +4,19 @@ import java.io.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+/**
+ * 类 FileSystemClassLoader 继承自类 java.lang.ClassLoader 。
+ * 在 java.lang.ClassLoader 类介绍 中列出的 java.lang.ClassLoader 类的常用方法中，
+ * 一般来说，自己开发的类加载器只需要覆写 findClass(String name) 方法即可。
+ * java.lang.ClassLoader 类的方法 loadClass() 封装了前面提到的代理模式的实现。
+ * 该方法会首先调用 findLoadedClass() 方法来检查该类是否已经被加载过；
+ * 如果没有加载过的话，会调用父类加载器的 loadClass() 方法来尝试加载该类；
+ * 如果父类加载器无法加载该类的话，就调用 findClass() 方法来查找该类。
+ * 因此，为了保证类加载器都正确实现代理模式，在开发自己的类加载器时，最好不要覆写 loadClass() 方法，而是覆写 findClass() 方法。
+ *
+ * 类 FileSystemClassLoader 的 findClass() 方法首先根据类的全名在硬盘上查找类的字节代码文件（.class 文件），
+ * 然后读取该文件内容，最后通过 defineClass() 方法来把这些字节代码转换成 java.lang.Class 类的实例。
+ */
 public class FileSystemClassLoader extends ClassLoader {
     private String rootDir;
 
