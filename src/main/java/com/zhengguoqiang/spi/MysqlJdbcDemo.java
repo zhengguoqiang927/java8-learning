c34c8231c451440f38af2f1e9fdd334ebfd6cf73
zhengguoqiang <zhengguoqiang927@gmail.com>
2020-12-10 12:59:02 +0800

    add spi

--- src/main/java/com/zhengguoqiang/spi/MysqlJdbcDemo.java
+++ src/main/java/com/zhengguoqiang/spi/MysqlJdbcDemo.java
@@ -11,14 +11,14 @@ public class MysqlJdbcDemo {
         PreparedStatement pstm = null;
         try {
             //1.注册驱动
-            Class.forName("com.mysql.jdbc.Driver");
+//            Class.forName("com.mysql.jdbc.Driver");
 
             //2.获取连接
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/seckill", "root", "root");
 
             //3.创建查询语句
-            pstm = connection.prepareStatement("select  * from test");
-        } catch (ClassNotFoundException | SQLException e) {
+            pstm = connection.prepareStatement("select  * from user ");
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
