2a73408b354c7a746f880e4a04c48f644620588e
zhengguoqiang <zhengguoqiang927@gmail.com>
2020-12-28 10:58:52 +0800

    add solution

--- src/test/java/com/zhengguoqiang/Solution_one.java
+++ src/test/java/com/zhengguoqiang/Solution_one.java
@@ -12,33 +12,32 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.PriorityBlockingQueue;
 import java.util.stream.Collectors;
 
-public class Solution {
-    //{"key":"A1","value":[0.5,0,3,1,1]}
+public class Solution_one {
+    //{"key":"A1","value":[0.5,0,3,1,1]}  -> 20m
     Map<String, List<Double>> seedMap = new HashMap<>();
 
-    //{"B1":"B1,0.5,0.3,0,1"}
+    //{"B1":[B1,0.5,0.3,0,1]"} -> 2g
     ConcurrentHashMap<String, List<Double>> allS = new ConcurrentHashMap<>();
 
-    //{"key":"A1","value":["B1","B2","B1"]}
-    Map<String, List<String>> allMap;
+    //{"key":"B1","value":["A1","A2","A1"]}
+    ConcurrentHashMap<String, List<String>> allMap;
 
     List<String> seedList;
 
+    //[[9NR263A8OPX47WU,7OVB65U218XWS0Q,0.6647528],[95RQ3T1WX0824UB,O61AT94S8URQXNV,0.66443107]]
     PriorityBlockingQueue<TargetObject> queue;
 
     int thresold;
 
     int startIndex = 129;
 
-    int maxLimit = 50;
+    int maxLimit = 100;
 
     class TargetObject implements Comparable<TargetObject> {
-        private String seed;
         private String target;
         private double score;
 
-        public TargetObject(String seed, String target, double score) {
-            this.seed = seed;
+        public TargetObject(String target, double score) {
             this.target = target;
             this.score = score;
         }
@@ -69,7 +68,7 @@ public class Solution {
 
         @Override
         public String toString() {
-            return "[" + seed + "," + target + "," + score + "]";
+            return "[" + target + "," + score + "]";
         }
     }
 
@@ -89,26 +88,29 @@ public class Solution {
         }
     }
 
-    //allMap:{"key":"A1","value":["B1","B2","B1","B2"]}
+    //allMap:{"key":"B1","value":["A1","A2","A1","A2"]}
     private void compute(String key, List<String> targets) {
         if (targets == null || targets.size() == 0) return;
         Map<String, Long> collect = targets.stream().collect(Collectors.groupingBy(s -> s, Collectors.counting()));
-        List<String> targetKeys = collect.entrySet().stream()
+        List<String> seedKeys = collect.entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed()).limit(maxLimit).map(Map.Entry::getKey).collect(Collectors.toList());
-        List<Double> seedValue = seedMap.get(key);
+        List<Double> targetKeys = allS.get(key);
 
-        for (String target : targetKeys) {
-            TargetObject compute = compute(key, target, seedValue, allS.get(target));
+        TargetObject max = null;
+        for (String seed : seedKeys) {
+            TargetObject compute = compute(key, seedMap.get(seed), targetKeys);
             if (compute == null) continue;
-            queue.add(compute);
-            if (queue.size() > thresold) {
-                queue.poll();
-            }
+            max = max == null ? compute : (compute.score > max.score ? compute : max);
         }
-
+        allMap.remove(key);
+        allS.remove(key);
+        queue.add(max);
+        if (queue.size() > thresold) {
+            queue.poll();
+        }
     }
 
-    private TargetObject compute(String seedKey, String targetKey, List<Double> seeds, List<Double> targets) {
+    private TargetObject compute(String targetKey, List<Double> seeds, List<Double> targets) {
         double fenzi = 0, fenmuA = 0, fenmuB = 0;
         for (int i = 0; i < seeds.size(); i++) {
             double a = seeds.get(i);
@@ -124,7 +126,7 @@ public class Solution {
         BigDecimal f = new BigDecimal(fenzi);
         double t = Math.sqrt(fenmuA) * Math.sqrt(fenmuB);
         BigDecimal m = new BigDecimal(t);
-        return new TargetObject(seedKey, targetKey, f.divide(m,8,BigDecimal.ROUND_HALF_UP).doubleValue());
+        return new TargetObject(targetKey, f.divide(m,8,BigDecimal.ROUND_HALF_UP).doubleValue());
     }
 
 
@@ -140,40 +142,21 @@ public class Solution {
         List<TargetObject> result = new ArrayList<>(outputCount);
 
         seedList = Files.readAllLines(Paths.get(seedFile));
-        queue = new PriorityBlockingQueue<>(outputCount);
+        queue = new PriorityBlockingQueue<>(outputCount/2);
         thresold = outputCount;
 
         //{"key":"A1","value":[0.5,0,3,1,1]}
         seedMap = new HashMap<>(seedList.size());
 
         //{"B1":[B1,0.5,0.3,0,1]}
-        allS = new ConcurrentHashMap<>(outputCount);
-        //{"key":"A1","value":["B1","B2","B1"]}
-        allMap = new HashMap<>();
+        allS = new ConcurrentHashMap<>();
+        //{"key":"B1","value":["A1","A2","A1"]}
+        allMap = new ConcurrentHashMap<>(outputCount/2);
 
-        //a[0]=[B1,B2,B3],a[1] = [B1,B2]
+        //a[0]=[A1,A2,A3],a[1] = [A1,A2]
         List<Set<String>> seedArray = new ArrayList<>(128);
         for (int i = 0; i < 128; i++) {
             seedArray.add(i, new HashSet<>());
-        }
-
-        List<CompletableFuture<Void>> futureList = new ArrayList<>();
-        try (BufferedReader bisAll = new BufferedReader(new InputStreamReader(new FileInputStream(allFile)), 10 * 1024 * 1024)) {
-            String s = null;
-            while ((s = bisAll.readLine()) != null) {
-                String[] split = s.split(",");
-                String id = split[0];
-                List<Double> targets = new ArrayList<>(256);
-                for (int i = 1; i < split.length; i++) {
-                    targets.add(Double.parseDouble(split[i]));
-                    if (i >= startIndex && Double.parseDouble(split[i]) == 1) {
-                        seedArray.get(i - startIndex).add(id);
-                    }
-                }
-                allS.put(id, targets);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
 
         for (String seed : seedList) {
@@ -183,17 +166,39 @@ public class Solution {
             for (int i = 1; i < split.length; i++) {
                 doubles.add(Double.parseDouble(split[i]));
                 if (i >= startIndex && Double.parseDouble(split[i]) == 1) {
-                    if (allMap.containsKey(id)) {
-                        allMap.get(id).addAll(seedArray.get(i - startIndex));
-                    } else {
-                        allMap.put(id, new ArrayList<>(seedArray.get(i - startIndex)));
-                    }
+                    seedArray.get(i - startIndex).add(id);
                 }
             }
             seedMap.put(id, doubles);
-            futureList.add(CompletableFuture.runAsync(new Task(id, allMap.get(id))));
-//                if (allMap.size() > 10000) allMap.clear();
         }
+        seedList = null;
+
+        List<CompletableFuture<Void>> futureList = new ArrayList<>();
+        try (BufferedReader bisAll = new BufferedReader(new InputStreamReader(new FileInputStream(allFile)), 5 * 1024 * 1024)) {
+            String s = null;
+            while ((s = bisAll.readLine()) != null) {
+                String[] split = s.split(",");
+                String id = split[0];
+                List<Double> targets = new ArrayList<>(256);
+                for (int i = 1; i < split.length; i++) {
+                    targets.add(Double.parseDouble(split[i]));
+                    if (i >= startIndex && Double.parseDouble(split[i]) == 1) {
+                        int index = i - startIndex;
+                        if (allMap.containsKey(id)) {
+                            allMap.get(id).addAll(seedArray.get(index));
+                        } else {
+                            allMap.put(id, new ArrayList<>(seedArray.get(index)));
+                        }
+                    }
+                }
+                allS.put(id, targets);
+                futureList.add(CompletableFuture.runAsync(new Task(id, allMap.get(id))));
+//                if (allMap.size() > 10000) allMap.clear();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
         CompletableFuture.allOf(futureList.toArray(new CompletableFuture[0])).join();
 
@@ -203,7 +208,7 @@ public class Solution {
         }
 
         Collections.reverse(result);
-        result.forEach(targetObject -> System.out.println(targetObject.toString()));
+        result.forEach(targetObject -> System.out.println(targetObject.toString() + ",size:" + targetObject.toString().getBytes().length));
         //请通过此方法输出答案,多次调用会追加记录
 //        MainFrame.addSet(result);
     }
@@ -211,9 +216,9 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         String seedFile = "/Users/zhengguoqiang/Downloads/java/input/seed.txt";
         String allFile = "/Users/zhengguoqiang/Downloads/java/input/all.txt";
-        Solution solution = new Solution();
+        Solution_one solution = new Solution_one();
         long start = System.currentTimeMillis();
-        solution.process(seedFile, allFile, 1000, null);
+        solution.process(seedFile, allFile, 20, null);
         System.out.println("time:" + (System.currentTimeMillis() - start));
 
 //        String seed = "A1,0.52,0.1,1,0";
